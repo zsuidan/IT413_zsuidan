5e0324d71b61176a5d079e990379713751a3492f
Zachary Suidan <zsuidan@mail.walshcollege.edu>
2021-10-18 19:02:28 -0400

    Finished JQuery Assignment

--- Week 3/JQuery Assignment/js/jquery_testing.js
+++ Week 3/JQuery Assignment/js/jquery_testing.js
@@ -2,9 +2,18 @@ $(document).ready(function() {
 
     $("body").append("<br><img id=\"other_sloth_image\" src=\"images/sloth2.jpg\">");
 
-    $("#other_sloth_image").css("border-radius", "50%")
-    $("#other_sloth_image").css("width", "50%")
-    $("#other_sloth_image").css("height", "50%")
+    $("#other_sloth_image").css("border-radius", "50%");
+    $("#other_sloth_image").css("width", "50%");
+    $("#other_sloth_image").css("height", "50%");
+
+    $("body").append("<ul>" + 
+    
+    "<li>Sloths have an extremely low metabolic rate, which means they move at a languid, sluggish pace through the trees. On average, sloths travel 41 yards per day.</li>" + 
+    "<li>Sloths snooze for about 15 hours per day. That leaves only nine hours to lumber through the trees. They maintain a low body temperature of about " + 
+    "86-93 F and move in and out of shade to regulate their body temperature.</li>" + 
+    "<li>Surprisingly, sloths are strong swimmers. They will sometimes drop down from their treetop perches into water and use their extended arms to propel through the water.</li>" + 
+    
+    "</ul>");
 
     $("#text_change_button").click(function() {
         $("#first_heading").html("Sloths are very cool");
@@ -15,7 +24,9 @@ $(document).ready(function() {
     })
 
     $("#paragraph_style_button").click(function() {
-        $(".normal").css("color", "purple");
         $(".normal").css("font-family", "Arial");
+        $("#first_paragraph").css("color", "red");
+        $("#second_paragraph").css("color", "green")
+        $("#third_paragraph").css("color", "blue")
     })
 })
