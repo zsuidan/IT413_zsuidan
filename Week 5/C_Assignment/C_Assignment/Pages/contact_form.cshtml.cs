7acc125be9cc1295cf5a6061b38c4b16d04e21ea
Zachary Suidan <zsuidan@mail.walshcollege.edu>
2021-11-01 05:42:37 -0400

    Finished C# assignment

---
+++ Week 5/C_Assignment/C_Assignment/Pages/contact_form.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace C_Assignment.Pages
+{
+    public class contact_formModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
